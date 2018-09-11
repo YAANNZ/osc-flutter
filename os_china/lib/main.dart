a0924ad8cb1aa7f4740edf70065469e48068787b
zynWindows <ynzhuw@gmail.com>
2018-09-11 22:09:03 +0800

    little

--- os_china/lib/main.dart
+++ os_china/lib/main.dart
@@ -23,50 +23,80 @@ import 'package:flutter/material.dart';
 //  }
 //}
 
-void main() => runApp(new MyStatefulWidget());
+//void main() => runApp(new MyStatefulWidget());
+//
+//class MyStatefulWidget extends StatefulWidget {
+//  @override
+//  State<StatefulWidget> createState() {
+//    return new MyStatefulWidgetState();
+//  }
+//}
+//
+//class MyStatefulWidgetState extends State<MyStatefulWidget> {
+//  String text = 'Click Me!';
+//
+//  changeText() {
+//    if (text == 'Click Me!') {
+//      setState(() {
+//        text = "Hello World!";
+//      });
+//    } else {
+//      setState(() {
+//        text = "Click Me!";
+//      });
+//    }
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return new MaterialApp(
+//      title: "Test StatefulWidget",
+//      theme: new ThemeData(
+//        // ignore: expected_token
+//        primarySwatch: Colors.red,
+//      ),
+//      home: new Scaffold(
+//        appBar: new AppBar(
+//          title: new Text("Test"),
+//        ),
+//        body: new Center(
+//          child: new InkWell(
+//            child: new Text(text),
+//            onTap: () {
+//              this.changeText();
+//            },
+//          )
+//        ),
+//      ),
+//    );
+//  }
+//}
 
-class MyStatefulWidget extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return new MyStatefulWidgetState();
-  }
+void main() {
+  runApp(new MyApp());
 }
 
-class MyStatefulWidgetState extends State<MyStatefulWidget> {
-  String text = 'Click Me!';
+// 有状态组件 MyApp
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new MyOSCClientState();
+}
 
-  changeText() {
-    if (text == 'Click Me!') {
-      setState(() {
-        text = "Hello World!";
-      });
-    } else {
-      setState(() {
-        text = "Click Me!";
-      });
-    }
-  }
-
+class MyOSCClientState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new MaterialApp(
-      title: "Test StatefulWidget",
       theme: new ThemeData(
-        // ignore: expected_token
-        primarySwatch: Colors.red,
+        primaryColor: const Color(0xFF63CA6C)
       ),
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text("Test"),
-        ),
-        body: new Center(
-          child: new InkWell(
-            child: new Text(text),
-            onTap: () {
-              this.changeText();
-            },
-          )
+          title: new Text("My OSC",
+            style: new TextStyle(color: Colors.white),
+          ),
+          iconTheme: new IconThemeData(color: Colors.white),
         ),
       ),
     );
